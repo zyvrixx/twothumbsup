88f91d1895a547acc8e97b308fbf292d837ee2a3
zyvrixx <zyvrixx@gmail.com>
2025-11-23 10:13:41 +0800

    testing api endpoints again

--- app/api/getblogpots/route.ts
+++ app/api/getblogpots/route.ts
@@ -1,14 +1,22 @@
 import {supabase } from "@/lib/supabase"
-import { quoteSchema } from "@/schema/models"
-import { supabaseServer } from "@/lib/server"
+// import { quoteSchema } from "@/schema/models"
+// import { supabaseServer } from "@/lib/server"
 import * as z from "zod"
-import { Quote } from "lucide-react"
+// import { Quote } from "lucide-react"
 
-type Quote = z.infer<typeof quoteSchema>
+// type Quote = z.infer<typeof quoteSchema>
 
 //fetching quote post from the db
+//import data from facebook then upsert 
+//cause i will repeat this request everyday
+
+
+const pageid = process.env.PAGE_ID!
+const pagetoken = process.env.PAGE_ACCESS_TOKEN!
+
 export async function GET (req : Request, res : Response) {
     try {
+    
     const {data, error} = await supabase.from("posts").select("*")    
     return Response.json(data)
     } catch(error) {
@@ -16,10 +24,13 @@ export async function GET (req : Request, res : Response) {
     }
     //replace this with a custom middleware to check for errors
 }
-
+/* 
 export async function POST(req :  Request) {
-    try {
-    const body = quoteSchema.parse( await req.json())
+   try {
+    const pageUrl = `https://graph.facebook.com/v18.0/${pageid}?fields=name,picture.type(large)&access_token=${PAGE_ACCESS_TOKEN}`;
+    const pageResp = await fetch(pageUrl);
+    const pageData = await pageResp.json();
+    pageData = quoteSchema.parse( await req.json())
     
     const {data, error} = await supabaseServer.from("post")
     .insert(body).select();
@@ -28,7 +39,13 @@ export async function POST(req :  Request) {
     } catch(error) {
         console.log(error)
     }
-
-    
-
 }
+
+
+export async function PATCH(req : Request) {
+    try {
+        const body = quoteSchema.parse(await req.json())
+        const {Title , Tags , ContentUrl } = body
+    }
+
+} */
