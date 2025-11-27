94e0e903fd2244d4a5ae425ade9e5c34d9959b37
zyvrixx <zyvrixx@gmail.com>
2025-11-27 16:06:45 +0800

    added basic http method on the backend

--- app/api/getblogpots/route.ts
+++ app/api/getblogpots/route.ts
@@ -1,4 +1,7 @@
 // import {supabase } from "@/lib/supabase"
+//
+import { supabaseServer } from "@/lib/server";
+import { NextResponse } from "next/server";
 // import { quoteSchema } from "@/schema/models"
 // import { supabaseServer } from "@/lib/server"
 // import * as z from "zod"
@@ -28,6 +31,7 @@ export async function POST(req :  Request) {
 }
 
 
+
 export async function PATCH(req : Request) {
     try {
         const body = quoteSchema.parse(await req.json())
@@ -35,3 +39,37 @@ export async function PATCH(req : Request) {
     }
 
 } */
+
+export async function POST (req : Request) {
+   const item = await req.formData() 
+
+   const title = item.get("title")
+    const tags = JSON.parse(item.get("tags") as string | "[]")
+    const file = item.get("file") as File;
+    
+    const fileBuffer = Buffer.from(await file.arrayBuffer())
+    const filePath = `twothumbsup/${Date.now()}-${file.name}`
+    
+   const {error} = await supabaseServer
+   .storage.from("twothumbsup").upload(filePath, fileBuffer, {
+    contentType : file.type
+   }) 
+
+    
+   const {error :insertError, data} = await supabaseServer.from("posts")
+   .insert({
+    title,
+    tags,
+    ImagePath : filePath
+   })
+
+   if (error) return NextResponse.json(error.message)
+
+    return NextResponse.json("upload successfull")
+}
+
+
+export async function GET() {
+    const {error, data} = await supabaseServer.from("post").select("*")
+    .order("created_at", {ascending : false})
+}
